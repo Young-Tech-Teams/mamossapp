51f9a38eabac0f490e88a85f8e18aab558ca81a0
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-17 17:53:11 +0100

    Profile page with user info modal

--- frontend/src/components/profile/InfoModal.jsx
+++ frontend/src/components/profile/InfoModal.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
+import { API_BASE_URL } from '../utils/APIRoutes';
 
 const InfoModal = ({ setShowModal }) => {
 
+  const token = JSON.parse(localStorage.getItem("token"));
+  
    const [values, setValues] = usesState({
       firstname: "",
       lastname: "",
@@ -13,6 +16,10 @@ const InfoModal = ({ setShowModal }) => {
       age: "",
       gender: ""
    })
+
+   const 
+
+   
 
    const handleChange = (e) => {
       setValues({ ...values, [e.target.name]: e.target.value });
