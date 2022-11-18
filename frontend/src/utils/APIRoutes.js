0dedd9a4e9103e1a45da964d933b8da5c291d47a
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-18 10:49:04 +0100

    Utils api user route

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -3,9 +3,11 @@ const port = "http://localhost:4242";
 const API_PUBLIC_URL = `${port}`;
 const API_BASE_URL = `${port}/api`;
 const API_AUTH_URL = `${port}/api/auth/`;
+const API_USER_URL = `${port}/api/user/`;
 
 export {
     API_PUBLIC_URL,
     API_BASE_URL,
     API_AUTH_URL,
+    API_USER_URL,
 }
