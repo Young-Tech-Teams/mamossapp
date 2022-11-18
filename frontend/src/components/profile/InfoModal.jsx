af5c2a4ef3ff7088eb2600910c16496b60db5a8e
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-18 09:33:07 +0100

    Profile page with user info modal

--- frontend/src/components/profile/InfoModal.jsx
+++ frontend/src/components/profile/InfoModal.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
-import { API_BASE_URL } from '../utils/APIRoutes';
+import { API_BASE_URL } from '../../utils/APIRoutes';
 
 const InfoModal = ({ setShowModal }) => {
 
   const token = JSON.parse(localStorage.getItem("token"));
   
-   const [values, setValues] = usesState({
+   const [values, setValues] = useState({
       firstname: "",
       lastname: "",
       email: "",
@@ -17,9 +17,57 @@ const InfoModal = ({ setShowModal }) => {
       gender: ""
    })
 
-   const 
+   const fetchCurrentUserInfo = () => {
+      var config = {
+        method: 'get',
+        url: `${API_BASE_URL}/user-infos`,
+        headers: localStorage.getItem("token") ? {
+          "Access-Control-Allow-Origin": "*",
+          "x-access-token": token,
+        } : {
+          "Access-Control-Allow-Origin": "*",
+        }
+      }
+  
+      axios.defaults.withCredentials = true;
+  
+      axios(config)
+      .then((response) => {
+         console.log(response);
+         console.log("It worked!");
+         setValues.firstname(response.data.firstname);
+         setValues.lastname(response.data.lastname);
+         setValues.email(response.data.email);
+         setValues.age(response.data.age);
+         setValues.gender(response.data.gender);
+      })
+      .catch((err) => {
+         console.log(err);
+      })
+   }
+      
+      useEffect(() => {
+        fetchCurrentUserInfo();
+      }, []);
 
-   
+
+   // var config = {
+   //    method: 'put',
+   //    url: `${API_BASE_URL}/user-update`,
+   //    headers: localStorage.getItem("token") ? {
+   //       "Access-Control-Allow-Origin": "*",
+   //       "x-access-token": token,
+   //    } : {
+   //       "Access-Control-Allow-Origin": "*"
+   //    }
+   // }
+
+   // axios.defaults.withCredetials = true;
+
+   // axios(config)
+   // .then((response) => {
+   //    setValues
+   // })
 
    const handleChange = (e) => {
       setValues({ ...values, [e.target.name]: e.target.value });
