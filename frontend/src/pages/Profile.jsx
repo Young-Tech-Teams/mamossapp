0f6a4d2277389ee05cc2dcd01d17e4847d4b46c5
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-23 15:42:31 +0100

    Profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -8,66 +8,19 @@ import Modal from '../components/profile/InfoModal';
 
 const Profile = () => {
 
-  const token = JSON.parse(localStorage.getItem("token"));
-  
-  const [data, setData] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    age: "",
-    gender: "",
-    createdAt: ""
-  })
   const [showModal, setShowModal] = useState(false);
 
-  const fetchCurrentUserInfo = () => {
-    const config = {
-      method: 'get',
-      url: `${API_USER_URL}infos`,
-      headers: localStorage.getItem("token") ? {
-        "Access-Control-Allow-Origin": "*",
-        "x-access-token": token,
-      } : {
-        "Access-Control-Allow-Origin": "*",
-      }
+  // MODAL
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    console.log("modal is working");
+  }
+  // Close modal with Echap Key
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      setShowModal(false);
     }
-
-    axios.defaults.withCredentials = true;
-
-    axios(config)
-      .then((response) => {
-        console.log(response);
-        console.log("It worked!");
-        setData({
-          ["firstname"]: response.data.firstname,
-          ["lastname"]: response.data.lastname,
-          ["email"]: response.data.email,
-          ["age"]: response.data.age,
-          ["gender"]: response.data.gender,
-          ["createdAt"]: response.data.createdAt
-        })
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    }
-
-    useEffect(() => {
-      fetchCurrentUserInfo();
-    }, []);
-
-    
-    // MODAL
-    const toggleModal = () => {
-      setShowModal(!showModal);
-      console.log("modal is working");
-    }
-    // Close modal with Echap Key
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
-        setShowModal(false);
-      }
-    });
+  });
     
   return (
     <section id="profile">
@@ -79,36 +32,7 @@ const Profile = () => {
           ) : <></>
         }
         <h2>Mon compte</h2>
-        <p>Bonjour {data.firstname}</p>
-
-        <div className="tabs">
-          <div className="delivery">
-            <a href="/mes-commandes">Commander un repas</a>
-          </div>
-        </div>
-
-        <div className="user-info d-flex">
-          <div className="avatar">
-            <img src="" alt="Avatar" />
-          </div>
-          <div className="bio">
-            <div className="name">
-              <span>{data.firstname ? data.firstname : "Prénom"}</span>
-              <hr />
-              <span>{data.lastname ? data.lastname : "Nom"}</span>
-            </div>
-            <div className="mail">
-              <span>{data.email ? data.email : "Votre email"}</span>
-            </div>
-            <div className="age gender">
-              <span>{data.age ? data.age : "Age"}</span>
-              <hr />
-              <span>{data.gender ? data.gender : "Sexe"}</span>
-            </div>
-            <div className="created">
-              <span>Mamossien depuis {data.createdAt ? data.createdAt : "??/??/??"}</span>
-            </div>
-          </div>
+        
 
           <div className="allergies">
             <span>Allergies</span>
