eeb147a25d196d66d2ab16ddb1aeb3a1d5511f1b
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-25 11:31:41 +0100

    address model

---
+++ backend/app/models/address/address.model.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, Sequelize) => {
+   const Address = sequelize.define("adresses", {
+       name: {
+           type: Sequelize.STRING
+       },
+       street: {
+           type: Sequelize.STRING
+       },
+       street_num: {
+           type: Sequelize.INTEGER
+       },
+       floor: {
+           type: Sequelize.STRING
+       },
+       door: {
+           type: Sequelize.INTEGER
+       },
+       building: {
+           type: Sequelize.STRING
+       },
+       code: {
+           type: Sequelize.INTEGER
+       },
+       zip_code: {
+           type: Sequelize.INTEGER
+       },
+       city: {
+           type: Sequelize.STRING
+       },
+       country_code: {
+           type: Sequelize.STRING
+       },
+       title: {
+           type: Sequelize.STRING
+       }
+   });
+   return Address;
+};
