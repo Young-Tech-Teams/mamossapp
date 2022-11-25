57fea1d99f410286d965f907eb574aa8aed4811d
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-25 16:20:54 +0100

    API Routes with Address & Payment URL

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -4,6 +4,8 @@ const API_PUBLIC_URL = `${port}`;
 const API_BASE_URL = `${port}/api`;
 const API_AUTH_URL = `${port}/api/auth/`;
 const API_USER_URL = `${port}/api/user/`;
+const API_ADDRESS_URL = `${port}/api/address/`;
+const API_PAYMENT_URL = `${port}/api/payment/`;
 
 export {
     API_PUBLIC_URL,
