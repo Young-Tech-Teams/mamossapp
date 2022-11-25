53623cb0e0ac9137c116496f38ccdb938103931b
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-25 13:00:31 +0100

    address routes

--- backend/app/routes/address/addressRoutes.js
+++ backend/app/routes/address/addressRoutes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
    router.post("/create", authJwt.verifyToken, addressController.create);
 
     // Retrieve a single adress with id
-    router.get("/list", addressController.findAddress);
+    router.get("/list/:id", addressController.findAddress);
 
     // Update a adress with id
     router.put("/update", authJwt.verifyToken, addressController.update);
