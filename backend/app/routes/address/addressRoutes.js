78069220ec5d82ddc9e00ec3dca520d78e625e2a
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-25 12:11:05 +0100

    Address routes

---
+++ backend/app/routes/address/addressRoutes.js
@@ -0,0 +1,47 @@
+const { authJwt } = require("../middleware");
+const addressController = require("../../controllers/address/address.controller");
+const router = require("express").Router();
+
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+    // Create a new adress
+    router.post("/create",
+        [
+            authJwt.verifyToken
+        ],
+        addressController.create
+      );
+
+    // Retrieve all adresss
+    router.get("/list",
+        [
+            authJwt.verifyToken
+        ],
+        addressController.findAll
+    );
+
+    // Retrieve a single adress with id
+    router.get("/list/:id",
+    addressController.findOne);
+
+    // Update a adress with id
+    router.put("/update/:id",
+        [
+            authJwt.verifyToken
+        ],
+        addressController.update);
+
+    // Delete a adress with id
+    router.delete("/delete/:id",
+        [
+            authJwt.verifyToken,
+        ],
+        addressController.delete);
+    app.use('/api/address', router);
+};
