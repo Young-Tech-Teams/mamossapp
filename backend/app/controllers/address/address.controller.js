a6c31b5b34f0cb4e532d00910bba309732b5302e
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-11-25 14:48:44 +0100

    address controller

--- backend/app/controllers/address/address.controller.js
+++ backend/app/controllers/address/address.controller.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const { TokenExpiredError } = jwt;
 const config = require("../../config/auth.config.js");
 
+// If token
+
+
 /**
 * @description Create and save a new address for current user
 * @param req
