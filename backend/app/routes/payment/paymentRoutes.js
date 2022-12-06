f6037772c1a3d3fe02d4f5a1473de5a072ac98dc
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-12-06 15:10:46 +0100

    Payments routes

--- backend/app/routes/payment/paymentRoutes.js
+++ backend/app/routes/payment/paymentRoutes.js
@@ -1,4 +1,4 @@
-const { authJwt } = require("../../middleware");
+const { verifyToken, isAdmin } = require("../../middleware");
 const paymentController = require("../../controllers/payment/payment.controller.js");
 const router = require("express").Router();
 
@@ -12,29 +12,29 @@ module.exports = function (app) {
    });
 
    // Create a new adress
-   router.post("/create", authJwt.verifyToken, paymentController.create);
+   router.post("/create", verifyToken, paymentController.create);
 
     // Retrieve current user adress with id
-    router.get("/list/:id", authJwt.verifyToken, paymentController.findOne);
+    router.get("/list/:id", verifyToken, paymentController.findOne);
 
     // Retrieve all adresses of current user
-    router.get("/list-all", authJwt.verifyToken, paymentController.findAll);
+    router.get("/list-all", verifyToken, paymentController.findAll);
 
     // Retrieve all adresses in database if admin
     router.get(
         "/get-all", 
         [
-            authJwt.verifyToken, 
-            authJwt.isAdmin
+            verifyToken, 
+            isAdmin
         ],
         paymentController.getAll
     );
     
     // Update a adress with id
-    router.put("/update/:id", authJwt.verifyToken, paymentController.update);
+    router.put("/update/:id", verifyToken, paymentController.update);
 
     // Delete a adress with id
-    router.delete("/delete/:id", authJwt.verifyToken, paymentController.delete);
+    router.delete("/delete/:id", verifyToken, paymentController.delete);
       
     app.use('/api/payment', router);
 };
