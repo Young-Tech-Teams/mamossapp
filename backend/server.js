56168aa25fc8e19981069427d7e4e912e21fca0c
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-12-06 16:25:20 +0100

    Cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ var path = require("path");
 var bcrypt = require("bcryptjs");
 
 app.use(cors({
-   origin: ["http://localhost:3000", "https://www.app.mamossa.com/connexion"],
+   origin: '*',
    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
    credentials: true,
 }));
