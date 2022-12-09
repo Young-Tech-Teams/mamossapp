95ad0fe1e02a0aeb453eafbaeff94141b088d100
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-12-09 17:17:34 +0100

    backend

--- backend/app/routes/userRoutes.js
+++ backend/app/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 module.exports = function (app) {
    app.use((req, res, next) => {
       res.header(
-         "Access-Control-Allow-Origin",
+         // "Access-Control-Allow-Origin",
          "Access-Control-Allow-Headers",
          "x-access-token, Origin, Content-Type, Accept"
       );
