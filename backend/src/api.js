6f1a0842aa0ae0e8aab2934a60de55f6a6cc7640
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-12-20 18:11:04 +0100

    Changing cors origin

--- backend/src/api.js
+++ backend/src/api.js
@@ -22,7 +22,7 @@ const corsOptions = {
 
 app.use(cors({
    corsOptions,
-   origin: "*",
+   origin: whitelist,
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true
